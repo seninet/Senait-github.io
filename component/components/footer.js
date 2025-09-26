e3b476a808fbbb1d659412eaa84a17da8d64b315
Senait <77326649+seninet@users.noreply.github.com>
2025-09-26 20:03:36 +0200

    Update footer.js

--- component/components/footer.js
+++ component/components/footer.js
@@ -29,7 +29,7 @@ class Footer extends HTMLElement {
            </div>
            <div class="icon">
              <a href="http://127.0.0.1:5503/index.html/hiding-email-addresses#unicode"></a>
-             <a href="https://github.com/Senait-coding" target="_blank"><i class="fa fa-github"></i></a>
+             <a href="https://github.com/seninet" target="_blank"><i class="fa fa-github"></i></a>
              <a href="https://www.linkedin.com/in/senait-b-86944277/" target="_blank"><i class="fa fa-linkedin"></i></a>
              <a href="https://www.instagram.com/yenejojo/"><i class="fa fa-instagram" target="_blank"></i></a>
              <a href="https://www.youtube.com/channel/UCzXfvDKpOJs6QexOd_uXP9Q" target="_blank"><i class="fa fa-youtube"></i></a>
